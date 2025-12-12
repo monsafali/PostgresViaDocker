934f0042b63af44eb3d83e9e21a880bc62c01a53
monsafali <monsafali272@gmail.com>
2025-12-12 08:41:48 +0500

    addiing method to connect postgres via neon and via docker

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,8 @@
-// import dotenv from "dotenv";
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
+
+
 // import pkg from "pg";
-
-
 // const { Pool } = pkg;
 
 // const pool = new Pool({
@@ -25,21 +25,48 @@
 
 
 
-import dotenv from "dotenv";
-dotenv.config();
-import pkg from "pg";
 
-const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.NEON_CONNECTION_STRING,
-  ssl: {
-    rejectUnauthorized: false, // Neon requires SSL
-  },
-});
+// import pkg from "pg";
 
-pool.on("connect", () => {
-  console.log("Connected to NeonDB successfully!");
-});
+// const { Pool } = pkg;
 
-export default pool;
+// const pool = new Pool({
+//   connectionString: process.env.NEON_CONNECTION_STRING,
+//   ssl: {
+//     rejectUnauthorized: false, // Neon requires SSL
+//   },
+// });
+
+// pool.on("connect", () => {
+//   console.log("Connected to NeonDB successfully!");
+// });
+
+// export default pool;
+
+
+
+
+
+
+
+// import pkg from "pg";
+// const { Client } = pkg;
+// const pool = new Client({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// try {
+//   await pool.connect();
+//   console.log("Database connected succesfuly");
+// } catch (error) {
+//   console.log("Somethign went wrong while connecting on database");
+//   console.log(error);
+//   process.exit(1);
+// }
+
+// export default pool;
